7ee8e93aea06626832411fbaeddd3920f4257604
kushagragupta <kushagra.gupta@clear.in>
2025-03-26 23:22:32 +0530

    add GetTasksByStatus in service impl

--- services/task_service_impl.go
+++ services/task_service_impl.go
@@ -23,8 +23,25 @@ func (s *TaskServiceImpl) GetTask(id string) (*models.Task, error) {
 	return s.repo.GetTask(id)
 }
 
-func (s *TaskServiceImpl) GetTasks() ([]models.Task, error) {
-	return s.repo.GetTasks()
+
+func (s *TaskServiceImpl) GetTasks(status string, limit int, offset int) ([]models.Task, error) {
+	if status != "" {
+		return s.repo.GetTasksByStatus(status)
+	} else {
+		tasks, err := s.repo.GetTasks()
+		if err != nil {
+			return nil, err
+		}
+
+		if limit > 0 {
+			if offset+limit > len(tasks) {
+				return tasks[offset:], nil
+			}
+			return tasks[offset : offset+limit], nil
+		}
+
+		return tasks, nil
+	}
 }
 
 func (s *TaskServiceImpl) UpdateTask(task *models.Task) error {
