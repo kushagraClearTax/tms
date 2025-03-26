57e3e23ac94b6c849cd414c3de9e3045d92c9d3e
kushagragupta <kushagra.gupta@clear.in>
2025-03-26 22:30:40 +0530

    added repo interface

---
+++ database/TaskRepository.go
@@ -0,0 +1,13 @@
+package database
+
+import (
+	"github.com/kushagragupta/tms/models"
+)
+
+type TaskRepository interface {
+	CreateTask(task *models.Task) error
+	GetTask(id string) (*models.Task, error)
+	GetTasks() ([]models.Task, error)
+	UpdateTask(task *models.Task) error
+	DeleteTask(id string) error
+}
