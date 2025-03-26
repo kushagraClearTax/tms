6b4bbe2e23b534cd5dce780e103cafd070c7225d
kushagragupta <kushagra.gupta@clear.in>
2025-03-26 23:24:18 +0530

    add offset limit and status in gettask

--- services/task_service.go
+++ services/task_service.go
@@ -7,7 +7,7 @@ import (
 type TaskService interface {
 	CreateTask(task *models.Task) error
 	GetTask(id string) (*models.Task, error)
-	GetTasks() ([]models.Task, error)
+	GetTasks(status string, limit int, offset int) ([]models.Task, error)
 	UpdateTask(task *models.Task) error
 	DeleteTask(id string) error
 }
